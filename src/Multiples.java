0c02184330a753162d2ad6b7a5aa4e285ebe9d87
Evelyn Hughes <evelyn.hughes@mail.utoronto.ca>
2024-09-09 14:04:15 -0400

    new method

---
+++ src/Multiples.java
@@ -0,0 +1,9 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int fives = 1000/5;
+        int threes = 1000/3;
+        int fifteens = 1000/15;
+        System.out.println(fives + threes - fifteens);
+
+    }
+}
